a1d4d0571a1682b3caa24372e070f50c03682f41
Trazz0 <nstau21@student.sdu.dk>
2023-11-18 01:07:01 +0100

    dimens.xml and colors.xml

--- app/src/main/java/com/example/android_project/screens/HomeScreen.kt
+++ app/src/main/java/com/example/android_project/screens/HomeScreen.kt
@@ -1,10 +1,24 @@
 package com.example.android_project.screens
 
+import androidx.annotation.Dimension
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.navigation.NavController
+import com.example.android_project.R
 import com.example.android_project.routes.Screen
 
 @Composable
@@ -17,6 +31,15 @@ fun HomeScreen(navigation: NavController){
     }
 }
 @Composable
-fun CreateGroup(){
-    Text(text = "something")
+fun CreateGroup() {
+    Box(
+        modifier = Modifier
+            .size(width = dimensionResource(R.dimen.group_width),height = dimensionResource(R.dimen.group_height))
+            .background(color = Color(R.color.gray))
+    ) {
+        Column( modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally){
+            Text(text = "You owe: 500kr")
+            Text(text = "Pay now")
+        }
+    }
 }
