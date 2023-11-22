ceac9140b09a3ecd5af0f4ffc5fd542d5d1e9289
weasy1as <habdu21@student.sdu.dk>
2023-11-22 17:34:49 +0100

    group page+ edit

--- app/src/main/java/com/example/android_project/screens/GroupEdit.kt
+++ app/src/main/java/com/example/android_project/screens/GroupEdit.kt
@@ -17,17 +17,91 @@ import androidx.navigation.NavController
 import com.example.android_project.ui.theme.Android_projectTheme
 
 @Composable
-fun GroupEdit() {
+fun GroupEdit(navController: NavHostController) {
+    var newPerson by remember { mutableStateOf("") }
+    var newAmount by remember { mutableStateOf("") }
+
     Surface(
-        modifier= Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize()
     ) {
-        Text(
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(80.dp),
-            text = "Group Page")
+                .fillMaxSize()
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState())
+                .background(Color.White)
+        ) {
+            // Top Text Element
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+                text = "Edit Group",
+                textAlign = TextAlign.Center
+            )
+
+            // Input Boxes for Extra Person and Amount
+            OutlinedTextField(
+                value = newPerson,
+                onValueChange = { newPerson = it },
+                label = { Text("New Person") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp)
+            )
+
+            OutlinedTextField(
+                value = newAmount,
+                onValueChange = { newAmount = it },
+                label = { Text("New Amount") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            )
+
+            // Buttons for Delete, Add Person, Confirm
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(
+                    onClick = {
+                        // Handle delete button click
+                        // For now, you can add your logic here
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp)
+                ) {
+                    Text(text = "Delete")
+                }
+
+                Button(
+                    onClick = {
+                        // Handle add person button click
+                        // For now, you can add your logic here
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp)
+                ) {
+                    Text(text = "Add Person")
+                }
+
+                Button(
+                    onClick = {
+                        // Handle confirm button click
+                        // For now, navigate back to the GroupPage
+                        navController.popBackStack()
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Confirm")
+                }
+            }
+        }
     }
 }
+
 
 @Preview
 @Composable
--- app/src/main/java/com/example/android_project/screens/GroupPage.kt
+++ app/src/main/java/com/example/android_project/screens/GroupPage.kt
@@ -1,16 +1,29 @@
 package com.example.android_project.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -21,16 +34,83 @@ import com.example.android_project.ui.theme.Android_projectTheme
 @Composable
 fun GroupPage(navController: NavHostController) {
     Surface(
-        modifier= Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize()
     ) {
-        Text(
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(80.dp),
-            text = "Group Page")
-    }
+                .fillMaxSize()
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+            // Top Text Elements
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+                text = "Full amount: 255 kr",
+                textAlign = TextAlign.Center
+            )
 
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+                text = "You owe: 46 kr",
+                textAlign = TextAlign.Center
+            )
+
+            // Rectangular Box with Text Elements
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp) // Adjusted height
+                    .background(Color.Gray, shape = RoundedCornerShape(16.dp)),
+                contentAlignment = Alignment.Center
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    // Text for "Person 1"
+                    Text(
+                        text = "Person 1",
+                        color = Color.Black
+                    )
+
+                    // Box with white background for "255 kr"
+                    Box(
+                        modifier = Modifier
+                            .background(Color.White, shape = RoundedCornerShape(16.dp))
+                            .padding(8.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = "255 kr",
+                            color = Color.Black
+                        )
+                    }
+                }
+            }
+
+            // Button to navigate to GroupEdit
+            Button(
+                onClick = {
+                    navController.navigate(Routes.EDIT_SCREEN)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+            ) {
+                Text(text = "Edit Group")
+            }
+        }
+    }
 }
+
+
+
+
+
 
 @Preview
 @Composable
