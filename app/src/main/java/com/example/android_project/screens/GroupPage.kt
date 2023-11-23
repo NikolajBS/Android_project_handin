304fdf1579d930efddbd4b9f38779438f3e3f746
weasy1as <habdu21@student.sdu.dk>
2023-11-23 19:18:55 +0100

    works

--- app/src/main/java/com/example/android_project/screens/GroupPage.kt
+++ app/src/main/java/com/example/android_project/screens/GroupPage.kt
@@ -2,14 +2,19 @@ package com.example.android_project.screens
 
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -21,6 +26,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -90,6 +96,7 @@ fun GroupPage(navController: NavController, groupId: String) {
             // Display group members
             for (member in groupMembers) {
                 GroupMemberItem(member)
+                Spacer(modifier = Modifier.height(8.dp))
             }
 
             // Button to navigate to GroupEdit
@@ -110,18 +117,40 @@ fun GroupPage(navController: NavController, groupId: String) {
 @Composable
 fun GroupMemberItem(member: GroupPerson?) {
     member?.let {
-        // Customize the UI for displaying each group member
-        Text(
+        // Rectangular Box with Text Elements for each group member
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(bottom = 8.dp),
-            text = "${it.name}: ${it.amount} kr",
-            style = MaterialTheme.typography.headlineMedium
-        )
+                .height(50.dp) // Adjusted height
+                .background(Color.Gray, shape = RoundedCornerShape(16.dp)),
+            contentAlignment = Alignment.Center
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                // Text for group member's name
+                Text(
+                    text = it.name,
+                    color = Color.Black
+                )
+
+                // Box with white background for the amount owed
+                Box(
+                    modifier = Modifier
+                        .background(Color.White, shape = RoundedCornerShape(16.dp))
+                        .padding(8.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "${it.amount} kr",
+                        color = Color.Black
+                    )
+                }
+            }
+        }
     }
 }
-
-
 
 @Preview
 @Composable
